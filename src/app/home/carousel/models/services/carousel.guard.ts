053b615f29cd897f3745444e48280438c361ae06
mehdi Qasemi <mehdi.q6991@gmail.com>
2025-10-21 17:01:49 +0330

    todo list route modified

--- src/app/home/carousel/models/services/carousel.guard.ts
+++ src/app/home/carousel/models/services/carousel.guard.ts
@@ -2,16 +2,18 @@ import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { CarouselService } from './carousel.service';
 import { firstValueFrom } from 'rxjs';
+import { ToastrService } from 'ngx-toastr';
 
 export const carouselGuard: CanActivateFn = async () => {
   const router = inject(Router);
   const carouselService = inject(CarouselService);
-
+  const toastr = inject(ToastrService);
   const slides = await firstValueFrom(carouselService.getSlides());
 
   if (slides.length === 0) {
     console.log("there isn't any data");
-    router.navigate(['/']);
+    toastr.error('داده ای برای نمایش وجود ندارد');
+    setTimeout(() => router.navigate(['/']), 5000);
     return false;
   }
 
--- src/app/home/todolist/todolist.component.ts
+++ src/app/home/todolist/todolist.component.ts
@@ -1,13 +1,36 @@
 import { Component } from '@angular/core';
-import { RouterLink } from "@angular/router";
+import { RouterLink } from '@angular/router';
+import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
+import { ITodo } from './models/interfaces/todo';
 
 @Component({
   selector: 'app-todolist',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './todolist.component.html',
-  styleUrl: './todolist.component.css'
+  styleUrl: './todolist.component.css',
 })
 export class TodolistComponent {
+  todoForm;
+  tasks: ITodo[] = [];
+  constructor(private fb: FormBuilder) {
+    this.todoForm = this.fb.group({
+      task: ['', Validators.required],
+    });
+  }
 
+  addTask() {
+    console.log('something added');
+    const taskValue = this.todoForm.value.task?.trim();
+    if (taskValue) {
+      this.tasks.push({ title: taskValue, done: false });
+      this.todoForm.reset();
+    }
+  }
+  removeTask(index: number) {
+    this.tasks.splice(index, 1);
+  }
+  toggleDone(task: { title: string; done: boolean }) {
+    task.done = !task.done;
+  }
 }
